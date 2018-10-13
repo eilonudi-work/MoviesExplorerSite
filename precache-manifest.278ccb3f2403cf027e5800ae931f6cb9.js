7d47250bca5a02622210f524e67f6637bbd9477c
Eilon <Eilon@Eilons-MacBook-Pro.local>
2018-10-14 02:42:45 +0300

    Updates

---
+++ precache-manifest.278ccb3f2403cf027e5800ae931f6cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab7505bd34514c5e9579b1a9630b2879",
+    "url": "/MoviesExplorerSite/static/media/error_image.ab7505bd.png"
+  },
+  {
+    "revision": "7a481c8f90db38676e0b",
+    "url": "/MoviesExplorerSite/static/js/runtime~main.7a481c8f.js"
+  },
+  {
+    "revision": "b1ebe87239df26926a68",
+    "url": "/MoviesExplorerSite/static/js/main.b1ebe872.chunk.js"
+  },
+  {
+    "revision": "2e8a40bbb53d90dc4882",
+    "url": "/MoviesExplorerSite/static/js/1.2e8a40bb.chunk.js"
+  },
+  {
+    "revision": "b1ebe87239df26926a68",
+    "url": "/MoviesExplorerSite/static/css/main.7fe945c2.chunk.css"
+  },
+  {
+    "revision": "c4ffb1b1204cb698db46360f8b30b043",
+    "url": "/MoviesExplorerSite/index.html"
+  }
+];
